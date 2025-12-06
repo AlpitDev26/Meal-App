c3d103c801fb1558fc44bce979dd893f01326e4a
Alpit Gadhave <alpitprogadhave6596@gmail.com>
2025-12-06 22:17:51 +0530

    Added fully visible detailed screen of each meal.

--- lib/screens/meals_detailed_screen.dart
+++ lib/screens/meals_detailed_screen.dart
@@ -22,22 +22,61 @@ class MealsDetailedScreen extends StatelessWidget {
         ),
       ),
       body: Scaffold(
-        body: Column(
-          children: [
-            Image.asset(
-              meals.imageUrl,
-              width: double.infinity,
-              fit: BoxFit.cover,
-              height: 250,
+        body: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Image.asset(
+                  meals.imageUrl,
+                  width: double.infinity,
+                  fit: BoxFit.cover,
+                  height: 250,
+                ),
+                const SizedBox(height: 20),
+                const Text(
+                  "Ingredients",
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    color: Colors.redAccent,
+                    fontSize: 22,
+                  ),
+                ),
+                SizedBox(height: 4),
+                ...meals.ingredients.map((item) {
+                  return Text(
+                    item,
+                    textAlign: TextAlign.left,
+                    style: TextStyle(color: Colors.amber, fontSize: 18),
+                  );
+                }),
+                const SizedBox(height: 15),
+                Text(
+                  "Steps",
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    color: Colors.redAccent,
+                    fontSize: 22,
+                  ),
+                ),
+                SizedBox(height: 4),
+                ...meals.steps.map((item) {
+                  return Padding(
+                    padding: const EdgeInsets.symmetric(
+                      horizontal: 8,
+                      vertical: 10,
+                    ),
+                    child: Text(
+                      item,
+                      textAlign: TextAlign.start,
+                      style: TextStyle(fontSize: 18, color: Colors.amber),
+                    ),
+                  );
+                }),
+              ],
             ),
-            const SizedBox(height: 20,),
-            const Text("Ingredients",
-            style: TextStyle(
-              fontWeight: FontWeight.bold,
-              color: Colors.redAccent,
-              fontSize: 22 
-            ),)
-          ],
+          ),
         ),
       ),
     );
